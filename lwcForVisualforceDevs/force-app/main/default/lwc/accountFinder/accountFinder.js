d9bf74fca85dc8803668dafaaaa8c509d66406a2
Naman Jain <naman199012@gmail.com>
2021-04-21 14:09:39 +0530

    lwcc

--- lwcForVisualforceDevs/force-app/main/default/lwc/accountFinder/accountFinder.js
+++ lwcForVisualforceDevs/force-app/main/default/lwc/accountFinder/accountFinder.js
@@ -1,5 +1,5 @@
-import { LightningElement } from 'lwc';
-
+import { LightningElement, wire } from 'lwc';
+import queryAccountsByRevenue from '@salesforce/apex/AccountListControllerLwc.queryAccountsByRevenue';
 export default class AccountFinder extends LightningElement {
     annualRevenue = null;
     handleChange(event){
@@ -8,4 +8,6 @@ export default class AccountFinder extends LightningElement {
     reset(){
         this.annualRevenue = null;
     }
+    @wire(queryAccountsByRevenue, { annualRevenue: '$annualRevenue' })
+    accounts;
 }
