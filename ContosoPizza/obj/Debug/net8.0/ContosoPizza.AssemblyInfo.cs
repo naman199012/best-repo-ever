ecd76d7cf105c276f0db526d59207c29e9cdc4b2
naman199012 <naman199012@gmail.com>
2026-05-17 17:28:51 +0530

    changes

--- ContosoPizza/obj/Debug/net8.0/ContosoPizza.AssemblyInfo.cs
+++ ContosoPizza/obj/Debug/net8.0/ContosoPizza.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3a8ee874ca7078461fb0b77159a05b118cbb9fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b00e16691a2126ed542267a5a38cbe5a13109086")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
