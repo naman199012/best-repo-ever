b50086c3afeb43f37186b2b65c00c79f57a10702
naman199012 <naman199012@gmail.com>
2026-05-18 18:22:33 +0530

    made few more changes

--- CalculateScores/Program.cs
+++ CalculateScores/Program.cs
@@ -93,16 +93,60 @@ Console.WriteLine($"{course5Name}\t\t{course5Grade}\t\t{course5Credit}");
 
 Console.WriteLine($"\nFinal GPA:\t\t{leadingDigit}.{firstDigit}{secondDigit}");
 
-Random dice = new();
-int roll1 = dice.Next();
-int roll2 = dice.Next(101);
-int roll3 = dice.Next(50, 101);
+Random diceA = new();
+int rollA = diceA.Next();
+int rollB = diceA.Next(101);
+int rollC = diceA.Next(50, 101);
 
-Console.WriteLine($"First roll: {roll1}");
-Console.WriteLine($"Second roll: {roll2}");
-Console.WriteLine($"Third roll: {roll3}");
+Console.WriteLine($"First roll: {rollA}");
+Console.WriteLine($"Second roll: {rollB}");
+Console.WriteLine($"Third roll: {rollC}");
 
 int firstNum = 10;
 int secondNum = 20;
 int largerNum = Math.Max(firstNum, secondNum);
 Console.WriteLine($"The larger number is: {largerNum}");
+
+Random dice = new();
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+{
+    if ((roll1 == roll2) && (roll2 == roll3))
+    {
+        Console.WriteLine("You rolled triples!  +6 bonus to total!");
+        total += 6;
+    }
+    else
+    {
+        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+        total += 2;
+    }
+
+    Console.WriteLine($"Your total including the bonus: {total}");
+}
+
+if (total >= 16)
+{
+    Console.WriteLine("You win a new car!");
+}
+else if (total >= 10)
+{
+    Console.WriteLine("You win a new laptop!");
+}
+else if (total == 7)
+{
+    Console.WriteLine("You win a trip for two!");
+}
+else
+{
+    Console.WriteLine("You win a kitten!");
+}
+
