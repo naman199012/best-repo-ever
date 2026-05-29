6900e571f6c3834be242341be14558d035bebd26
naman199012 <naman199012@gmail.com>
2026-05-29 12:29:25 +0530

    added pythonfile

---
+++ Pythonprojects/helloWorld.py
@@ -0,0 +1,11 @@
+print("Hello, World!")
+add = 5 + 3
+print("The sum of 5 and 3 is:", add)
+the_world_is_flat = False
+if the_world_is_flat:
+    print("The world is flat.")
+else:    
+    print("The world is not flat.")
+print(r"Hello\n'World'")  # This will print the string with a newline character
+print("Hello\nWorld")   # This will print "Hello" and "World" on separate lines
+print(rf"The value of add is\ {add}")  # Using an f-string to include the value of add in the output
